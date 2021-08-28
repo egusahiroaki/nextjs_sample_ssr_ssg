f6932795372dcae6a42fcfba629a5f4e3f31ca79
egusahiroaki <egusa.hiroaki@gmail.com>
2021-08-28 17:36:03 +0900

    add fetch in client side

--- pages/shows/[id].js
+++ pages/shows/[id].js
@@ -1,14 +1,42 @@
 import fetch from "isomorphic-unfetch";
 
-const Post = (props) => (
-  <div>
-    <h1>{props.show.name}</h1>
-    <p>{props.test}</p>
-    <p>{props.buildtime}</p>
-    <p>{props.show.summary}</p>
-    {props.show.image ? <img src={props.show.image.medium} /> : null}
-  </div>
-);
+const Post = (props) => {
+  const click = () => {
+    console.log("click");
+    console.log(props.show.id);
+    fetch(`https://api.tvmaze.com/shows/${props.show.id}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log("result");
+          console.log(result);
+          //   this.setState({
+          //     isLoaded: true,
+          //     items: result.items,
+          //   });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          //   this.setState({
+          //     isLoaded: true,
+          //     error,
+          //   });
+        }
+      );
+  };
+  return (
+    <div>
+      <h1>{props.show.name}</h1>
+      <p>{props.test}</p>
+      <p>{props.buildtime}</p>
+      <p>{props.show.summary}</p>
+      {props.show.image ? <img src={props.show.image.medium} /> : null}
+      <button onClick={click}>取得</button>
+    </div>
+  );
+};
 
 export async function getServerSideProps(context) {
   console.log(`個別ページ getServerSideProps`);
